03a6ac3e9f8489d24ffae934978356ff638415a7
Bradley Comerford <bradley@saplingservers.net>
2023-12-15 02:36:02 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 $config = array(
     'port' => '3493',               /* Port of NUT Server */
-    'server' => '10.20.10.191',        /* NUT Server */
-    'ups_name' => 'eaton3s',            /* UPS name configured in ups.conf */
-    'title' => 'Eaton3S UPS Web Status',    /* Script title */
+    'server' => '127.0.0.1',        /* NUT Server */
+    'ups_name' => 'ups',            /* UPS name configured in ups.conf */
+    'title' => 'NUT Web Server',    /* Script title */
     'watt' => '550',                /* UPS capacity in Watt */
-    'copyright' => 'dezr.in',     /* Copyright */
+    'copyright' => 'Dezrin',     /* Copyright */
     'version' => '1.00.3',          /* Version */
     'debug' => 'true',
     );
