f06425d3e6d87e3e2084cbf3c02182a09a1c2f81
Bradley Comerford <bradley@saplingservers.net>
2023-12-15 11:37:17 +0000

    Update footer

--- index.php
+++ index.php
@@ -220,9 +220,18 @@ fclose($fp);
 			</div>
 		<br />
 		</div>
-		<hr>
-		<div class="footer">
-			<p>&copy; <?php echo date('Y'); ?> by <?php echo $config['copyright']; ?></p>
+		<div class="container">
+			<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+				<div class="col-md-4 d-flex align-items-center">
+				<span class="mb-3 mb-md-0 text-muted">© <?php echo date('Y') . ' ' . $config['copyright']; ?></span>
+				</div>
+
+				<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+				<li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+				<li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+				<li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+				</ul>
+			</footer>
 		</div>
 	</body>
 	<!-- Bootstrap core JS-->
